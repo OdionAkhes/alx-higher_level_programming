532a8c889470ff8b52cce4d6eafcb44266f10f2d
Akhetuamhen Odion <akhetuamhenodion@gmail.com>
2022-05-31 06:16:37 -0700

    11

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10 if number >= 0 else -number % 10
-    print(f"{last_digit}", end="")
-    return 
+    if (number >= 0):
+        last = number % 10
+    else:
+        last = number * -1 % 10
+    print("{:d}".format(last), end="")
+    return (last)
